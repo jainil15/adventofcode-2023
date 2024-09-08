cfba62650dee044928d4e45e187068d0db8597ea
Jainil <jainilpatel115@gmail.com>
2024-09-08 15:26:50 +0530

    Add: 01 Complete, But bad performance

---
+++ 01_Trebuchet/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func fileReader(path string) (string, error) {
+	inBytes, err := os.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return string(inBytes), nil
+}
+
+func getBlock(input string) []string {
+	blocks := strings.Split(input, "\n\n")
+	return blocks
+}
+
+func getLine(input string) []string {
+	line := strings.Split(input, "\n")
+	return line[:len(line)-1]
+}
+func isNumeric(w string) bool {
+	_, err := strconv.Atoi(w)
+	return err == nil
+}
+
+func getCalibrationValue(line string) int {
+	var calibrationValueString string
+	n := len(line)
+	for _, word := range line {
+		if isNumeric(string(word)) {
+			calibrationValueString = string(word)
+			break
+		}
+	}
+
+	for i, _ := range line {
+		if isNumeric(string(line[n-1-i])) {
+			calibrationValueString += string(line[n-1-i])
+			break
+		}
+	}
+	calibrationValue, err := strconv.Atoi(calibrationValueString)
+	if err != nil {
+		panic(err)
+	}
+	return calibrationValue
+}
+func main() {
+	path := os.Args[1]
+	finalCalibrationValue := 0
+	content, err := fileReader(path)
+	if err != nil {
+		panic(err)
+	}
+	blocks := getBlock(content)
+	fmt.Println(len(blocks))
+	for _, block := range blocks {
+		lines := getLine(block)
+		for _, line := range lines {
+			finalCalibrationValue += getCalibrationValue(line)
+		}
+	}
+	fmt.Println(finalCalibrationValue)
+}
