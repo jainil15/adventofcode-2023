d0851ef27c1d16f69fc391b7a383ee56c0c7fed5
Jainil <jainilpatel115@gmail.com>
2024-09-13 14:36:57 +0530

    Add: 06 part2 under 200us

--- 06_Wait_For_It/main.go
+++ 06_Wait_For_It/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
 	"regexp"
 	"strconv"
@@ -25,6 +26,23 @@ func convertToInt(input []string) []int {
 		}
 		output = append(output, o)
 	}
+	return output
+}
+func parseInput2(input string) Input {
+	var output Input
+	lines := strings.Split(input, "\n")
+	time := numberRegex.FindAllString(lines[0], -1)
+	distance := numberRegex.FindAllString(lines[1], -1)
+	t, err := strconv.Atoi(strings.Join(time, ""))
+	if err != nil {
+		panic(err)
+	}
+	d, err := strconv.Atoi(strings.Join(distance, ""))
+	if err != nil {
+		panic(err)
+	}
+	output.time = []int{t}
+	output.distance = []int{d}
 	return output
 }
 func parseInput(input string) Input {
@@ -51,18 +69,36 @@ func calculateRange(distance int, time int) []int {
 	for i := 0; i < time; i++ {
 		if distance < (i * (time - i)) {
 			output = append(output, i)
-			break
-		}
-	}
-	for i := time; i > 0; i-- {
-		if distance < (i * (time - i)) {
-			output = append(output, i)
+			output = append(output, time-i)
 			break
 		}
 	}
 	return output
 }
+
+func calculateRange2(distance int, time int) []int {
+	// equation: output = t + (t^2 - 4d)^0.5 / 2
+	distanceF := float64(distance)
+	timeF := float64(time)
+	output := int(math.Ceil((timeF - math.Sqrt(timeF*timeF-4*distanceF)) / 2))
+	return []int{output, time - output}
+}
+
+func part2(input Input) int {
+
+	fmt.Println(input)
+	prod := 1
+	for i := 0; i < len(input.time); i++ {
+		time := input.time[i]
+		distance := input.distance[i]
+		rangeTime := calculateRange2(distance, time)
+		diff := calculateDiff(rangeTime[0], rangeTime[1])
+		prod *= diff
+	}
+	return prod
+}
 func part1(input Input) int {
+
 	fmt.Println(input)
 	prod := 1
 	for i := 0; i < len(input.time); i++ {
@@ -79,7 +115,7 @@ func part1(input Input) int {
 func main() {
 	start := time.Now()
 	path := os.Args[1]
-	input := parseInput(readFile(path))
-	fmt.Println("Part 1: ", part1(input))
+	input := parseInput2(readFile(path))
+	fmt.Println("Part 1: ", part2(input))
 	fmt.Println("Time: ", time.Since(start))
 }
